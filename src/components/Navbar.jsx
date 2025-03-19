0235b79a3507d392c01f2f3692a93741c16d6b4e
Pet3r <pierregourinat@gmail.com>
2025-03-19 20:27:58 +0100

    feat : navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export const Navbar = () => {
   return (
     <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
       <div className="container px-4 mx-auto relative text-sm">
-        <div className="flex justify-center items-center">
+        <div className="flex justify-between items-center">
           <div className="flex items-center flex-shrink-0">
             <img className="h-10 w-10 mr-2" src={logo} alt="logo" />
             <span className="text-xl tracking-tight">VirtualR</span>
@@ -17,6 +17,17 @@ export const Navbar = () => {
               </li>
             ))}
           </ul>
+          <div className="hidden lg:flex justify-center space-x-12 items-center">
+            <a href="#" className="py-2 px-3 border rounded-md">
+              Sign In
+            </a>
+            <a
+              href="#"
+              className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md"
+            >
+              Create an account
+            </a>
+          </div>
         </div>
       </div>
     </nav>
