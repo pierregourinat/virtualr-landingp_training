7be8142848349fb714fec3290285b0eb63caa45a
Pet3r <pierregourinat@gmail.com>
2025-03-21 13:53:34 +0100

    feat : featureSection

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { FeatureSection } from "./components/FeatureSection";
 import { HeroSection } from "./components/HeroSection";
 import { Navbar } from "./components/Navbar";
 import "./index.css";
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <HeroSection />
+        <FeatureSection />
       </div>
     </>
   );
